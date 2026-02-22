6b1e2fe899163619dbd134027961335fabee018b
joggy <joggymina@gmail.com>
2026-02-22 09:49:29 +0300

    recent update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,16 +20,16 @@ function generateMaskedPhone(): string {
 export default function FulizaBoostPage() {
   const limits = React.useMemo(
     () => [
-      { amount: 2700, fee: 59 },
-      { amount: 5000, fee: 81 },
-      { amount: 10000, fee: 198 },
-      { amount: 16500, fee: 290 },
-      { amount: 21000, fee: 420 },
-      { amount: 32000, fee: 590 },
-      { amount: 44000, fee: 789 },
-      { amount: 53000, fee: 990 },
-      { amount: 62000, fee: 1240 },
-      { amount: 75000, fee: 1880 },
+      { amount: 2700, fee: 69 },
+      { amount: 5000, fee: 91 },
+      { amount: 10000, fee: 199 },
+      { amount: 16500, fee: 377 },
+      { amount: 21000, fee: 481 },
+      { amount: 32000, fee: 598 },
+      { amount: 44000, fee: 797 },
+      { amount: 53000, fee: 999 },
+      { amount: 62000, fee: 1250 },
+      { amount: 75000, fee: 1890 },
     ],
     []
   );
